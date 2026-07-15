c1f11e8bb7fdb9c97d431b1cdac69b5e28b8f9ac
Duncan Lisutsa <duncanlisutsa@gmail.com>
2026-07-15 23:18:56 +0300

    Add logging config to surface server errors in production logs

--- config/settings.py
+++ config/settings.py
@@ -171,3 +171,25 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
