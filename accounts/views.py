4773d2d5b3f6e5062a8c50e05a8f17779015a286
Duncan Lisutsa <duncanlisutsa@gmail.com>
2026-07-26 13:58:59 +0300

    Debugging the Add landlord

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
